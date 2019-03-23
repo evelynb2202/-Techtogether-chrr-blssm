1c6c887c0394a6fb2981d4e99fd215fc034b3e64
Elizabete Mezinska <liz@em-thinkpad.amherst.edu>
2019-03-23 15:12:27 -0400

    Added overlay code, doesn't work. --liz

---
+++ overlay.js
@@ -0,0 +1,4 @@
+var odiv=document.createElement("div"); //creates the overlay div (odiv)
+odiv.className = "chrr-blssm-extension-overlay";
+document.body.appendChild(odiv); 
+odiv.innerText="test123";
