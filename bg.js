e1a7b5e40e6454dc39cca6ee1c893d9ce8e3e4e0
Elizabete Mezinska <liz@em-thinkpad.amherst.edu>
2019-03-24 05:15:01 -0400

    frontend nice backend mess

---
+++ bg.js
@@ -0,0 +1,31 @@
+//gets from storage or initializes array of past moods
+if(chrome.storage.local.get("moodHistoryStored") == true) {
+	var moodHistory = chrome.storage.local.get("moodHistoryStored");
+} else {
+	var moodHistory = new Array();
+}
+
+//interval between asking for mood
+if(chrome.storage.local.get("intervalStored") == true) {
+	var interval = chrome.storage.local.get("intervalStored");
+} else {
+	var interval = 86400000; // 24h in milliseconds
+}
+
+//last time asked, if none found, defaults to date decades ago
+if(chrome.storage.local.get("lastAskedStored") == true) {
+	var lastAsked = chrome.storage.local.get("lastAskedStored");
+} else {
+	var lastAsked = 0; //Jan 1, 1970, Unix epoch
+}
+
+var lastAsked = get("lastAskedStored");
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    moodHistory.push({time:Date.now, mood:request.answer1});
+    chrome.storage.local.set("moodHistoryStored": moodHistory);
+  });
--- overlay.js
+++ overlay.js
@@ -1,8 +1,11 @@
+
+//Main overlay box, set up for question 1
 var odiv=document.createElement("div"); //creates the overlay div (odiv)
 odiv.className = "chrr-blssm-ext-ol";
 document.body.appendChild(odiv); 
-console.log("ol runs");
+odiv.style.backgroundImage = "url(" + chrome.extension.getURL('chr-blssm-q1.png') + ")";
 
+//close button
 // var close=document.createElement("div");
 // close.className ="chrr-blssm-close-bt";
 // close.innerText="X";
@@ -14,25 +17,24 @@ console.log("ol runs");
 
 
 
-odiv.style.backgroundImage = "url(" + chrome.extension.getURL('chr-blssm-q1.png') + ")";
-
+//left answer button set up for question 1
 var btL=document.createElement("div");
 btL.className = "chr-blssm-bt-left";
 btL.style.backgroundImage = "url(" + chrome.extension.getURL('h1.png') + ")";
 btL.style.display = "block";
 btL.onclick = function() {
-	
+	chrome.runtime.sendMessage({answer1: "good"});
 	odiv.style.display = "none";
 };
 odiv.appendChild(btL);
 
-
+//right answer button set up for question 1
 var btR=document.createElement("div");
 btR.className = "chr-blssm-bt-right";
 btR.style.backgroundImage = "url(" + chrome.extension.getURL('s1.png') + ")";
 btR.style.display = "block";
 btR.onclick = function() {
-	
+	chrome.runtime.sendMessage({answer1: "bad"});
 	odiv.style.display = "none";
 };
 odiv.appendChild(btR);
