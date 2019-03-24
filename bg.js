c3fb5763f950e9aafdf14b79de455192b756ddbc
captainozlem <ozlemniksch@gmail.com>
2019-03-24 08:02:28 -0400

    last changes

--- bg.js
+++ bg.js
@@ -1,34 +1,66 @@
-// //gets from storage or initializes array of past moods
-// if(chrome.storage.local.get(["moodHistoryStored"]) == true) {
-// 	var moodHistory = chrome.storage.local.get(["moodHistoryStored"]);
-// } else {
-// 	var moodHistory = new Array();
-// }
+//gets from storage or initializes array of past moods
 
-// //interval between asking for mood
-// if(chrome.storage.local.get(["intervalStored"]) == true) {
-// 	var interval = chrome.storage.local.get(["intervalStored"]);
-// } else {
-// 	var interval = 86400000; // 24h in milliseconds
-// }
+var moodHistory = new Array();
 
-// //last time asked, if none found, defaults to date decades ago
-// if(chrome.storage.local.get(["lastAskedStored"]) == true) {
-// 	var lastAsked = chrome.storage.local.get(["lastAskedStored"]);
-// } else {
-// 	var lastAsked = 0; //Jan 1, 1970, Unix epoch
-// }
+chrome.storage.local.get(['moodHistoryStored'], function(result) {
+  if (typeof result.moodHistoryStored !== 'undefined') {
+    moodHistory = result.moodHistoryStored;
+    console.log(moodHistory);
+  } else {
+    console.log('hist initd');
+  }
+});
+console.log('a');
+console.log(moodHistory);
 
-// var lastAsked = get("lastAskedStored");
+console.log('b');
 
-// chrome.runtime.onMessage.addListener(
-//   function(request, sender, sendResponse) {
-//     console.log(sender.tab ?
-//                 "from a content script:" + sender.tab.url :
-//                 "from the extension");
-//     lastAsked = Date.now;
-//     chrome.storage.local.set({"lastAskedStored": lastAsked});
-//     moodHistory.push({time:Date.now, mood:request.answer1});
-//     chrome.storage.local.set({"moodHistoryStored": moodHistory});
-//     console.log(moodHistory);
-//   });
+//interval between asking for mood
+
+var interval = 86400000; // 24h in milliseconds
+
+chrome.storage.local.get(['intervalStored'], function(result) {
+  if (typeof result.intervalStored !== 'undefined') {
+    interval = result.intervalStored;
+    console.log(interval);
+  } else {
+    console.log('int initd');
+  }
+  chrome.storage.local.set({ intervalStored: interval });
+});
+
+//last time asked, if none found, defaults to date decades ago
+var lastAsked = 0; //Jan 1, 1970, Unix epoch
+
+chrome.storage.local.get(['lastAskedStored'], function(result) {
+  if (typeof result.lastAskedStored !== 'undefined') {
+    lastAsked = result.lastAskedStored;
+    console.log(lastAsked);
+  } else {
+    console.log('lastask inited');
+  }
+});
+
+//listen for messages
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  var d = new Date();
+
+  if (typeof request.answer1 !== 'undefined') {
+    lastAsked = d.getTime();
+    chrome.storage.local.set({ lastAskedStored: lastAsked });
+    moodHistory.push({ time: lastAsked, mood: request.answer1 });
+    chrome.storage.local.set({ moodHistoryStored: moodHistory });
+    console.log('answer noted');
+  }
+
+  if (typeof request.show !== 'undefined') {
+    if (lastAsked + interval < d.getTime()) {
+      sendResponse({ show: true });
+      console.log(lastAsked);
+      console.log(d.getTime());
+    } else {
+      sendResponse({ show: false });
+      console.log('show: f');
+    }
+  }
+});
--- overlay.js
+++ overlay.js
@@ -2,23 +2,17 @@
 var odiv = document.createElement('div'); //creates the overlay div (odiv)
 odiv.className = 'chrr-blssm-ext-ol';
 document.body.appendChild(odiv);
-
-//If answer the question, close the overlaybox
-function close() {
-  odiv.style.display = 'none';
-}
-
-// let today = new Date();
-// document.getElementById('demo').innerHTML = today.getDate();
-
-// if (today) {
-//   document.body.appendChild(odiv);
-// } else if (!this.btL.onclick || !this.btR.onclick) {
-//   setTimeout(close(), 100);
-// }
-
 odiv.style.backgroundImage =
   'url(' + chrome.extension.getURL('chr-blssm-q1.png') + ')';
+
+odiv.style.display = 'none';
+
+chrome.runtime.sendMessage({ show: true }, function(response) {
+  if (response.show == true) {
+    odiv.style.display = 'block';
+  }
+  console.log('show?' + response.show);
+});
 
 //left answer button set up for question 1
 var btL = document.createElement('div');
