cc520ac235c5a61ec882ef76b0652ea458914855
Elizabete Mezinska <liz@em-thinkpad.amherst.edu>
2019-03-23 23:55:36 -0400

    q1 bg added

--- overlay.js
+++ overlay.js
@@ -1,7 +1,6 @@
 var odiv=document.createElement("div"); //creates the overlay div (odiv)
 odiv.className = "chrr-blssm-ext-ol";
 document.body.appendChild(odiv); 
-odiv.innerText="test123";
 console.log("ol runs");
 
 var close=document.createElement("div");
@@ -12,3 +11,9 @@ close.onclick = function() {
 	odiv.style.display = "none";
 };
 odiv.appendChild(close);
+
+
+
+var q1url = "url(" + chrome.extension.getURL('chrr-blssm-q1.png') + ")"
+console.log(q1url);
+odiv.style.backgroundImage = q1url;
