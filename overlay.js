2da5ba24b53c3b28248f8e8345f3b7f416ea50d4
Elizabete Mezinska <liz@em-thinkpad.amherst.edu>
2019-03-23 23:40:27 -0400

    close button (just x no design)

--- overlay.js
+++ overlay.js
@@ -3,3 +3,12 @@ odiv.className = "chrr-blssm-ext-ol";
 document.body.appendChild(odiv); 
 odiv.innerText="test123";
 console.log("ol runs");
+
+var close=document.createElement("div");
+close.className ="chrr-blssm-close-bt";
+close.innerText="X";
+close.style.cursor - 'pointer';
+close.onclick = function() {
+	odiv.style.display = "none";
+};
+odiv.appendChild(close);
