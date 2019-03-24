1d87232ab4c9e977080f9efc014fc227dad12db8
Elizabete Mezinska <liz@em-thinkpad.amherst.edu>
2019-03-24 00:39:27 -0400

    asks one question does nothing with answer

--- overlay.js
+++ overlay.js
@@ -3,17 +3,36 @@ odiv.className = "chrr-blssm-ext-ol";
 document.body.appendChild(odiv); 
 console.log("ol runs");
 
-var close=document.createElement("div");
-close.className ="chrr-blssm-close-bt";
-close.innerText="X";
-close.style.cursor - 'pointer';
-close.onclick = function() {
+// var close=document.createElement("div");
+// close.className ="chrr-blssm-close-bt";
+// close.innerText="X";
+// close.style.cursor - 'pointer';
+// close.onclick = function() {
+// 	odiv.style.display = "none";
+// };
+// odiv.appendChild(close);
+
+
+
+odiv.style.backgroundImage = "url(" + chrome.extension.getURL('chr-blssm-q1.png') + ")";
+
+var btL=document.createElement("div");
+btL.className = "chr-blssm-bt-left";
+btL.style.backgroundImage = "url(" + chrome.extension.getURL('h1.png') + ")";
+btL.style.display = "block";
+btL.onclick = function() {
+	
 	odiv.style.display = "none";
 };
-odiv.appendChild(close);
+odiv.appendChild(btL);
 
 
-
-var q1url = "url(" + chrome.extension.getURL('chrr-blssm-q1.png') + ")"
-console.log(q1url);
-odiv.style.backgroundImage = q1url;
+var btR=document.createElement("div");
+btR.className = "chr-blssm-bt-right";
+btR.style.backgroundImage = "url(" + chrome.extension.getURL('s1.png') + ")";
+btR.style.display = "block";
+btR.onclick = function() {
+	
+	odiv.style.display = "none";
+};
+odiv.appendChild(btR);
